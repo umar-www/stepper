7a5ed140681a645a0bb711cc4f56987d352e1d7f
Pa1vanov_07 <pa1vanovmuhammadzohid.07@gmail.ru>
2023-03-13 15:30:25 +0500

    add init codes for html and js

---
+++ index.js
@@ -0,0 +1,5 @@
+const timeInput = document.getElementById("time");
+const stepInput = document.getElementById("step");
+
+const incrementBtn = document.getElementById("increment");
+const decrementBtn = document.getElementById("decrement");
